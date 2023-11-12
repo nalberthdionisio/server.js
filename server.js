bc5f94ecda9e48c7e37d548153cc55f1167a1b08
nalberthdionisio <nalberthdionisio@aluno.fapce.edu.br>
2023-11-12 10:35:32 -0300

    revertendo alterações

--- server.js
+++ server.js
@@ -4,17 +4,7 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
-
-server.use((req, res, next) => {
-  if (req.method === 'POST' || req.method === 'PUT' || req.method === 'PATCH') {
-    router.db.write();
-  }
-  next();
-});
-
 server.use(router);
-
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running');
 });
