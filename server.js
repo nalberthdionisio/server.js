b54ba0189e07fa6cda3a35cd94c74185b3ddb0bd
nalberthdionisio <nalberthdionisio@aluno.fapce.edu.br>
2023-11-12 10:32:27 -0300

    bd persistente

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.use((req, res, next) => {
+  if (req.method === 'POST' || req.method === 'PUT' || req.method === 'PATCH') {
+    router.db.write();
+  }
+  next();
+});
+
 server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running')
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running');
 });
